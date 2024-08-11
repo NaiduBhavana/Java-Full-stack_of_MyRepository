e6f28dddb1a41e29021cf1ec6b32b65ec60ced7b
NaiduBhavana <123732813+NaiduBhavana@users.noreply.github.com>
2024-08-11 09:03:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package org.studyeasy;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		//Java is a programming language full of classes and objects.Here we are making use of objects.
+		Car c = new Car();
+		c.speed = 40;
+		System.out.println(c.speed);
+		
+
+	}
+
+}
