ddae3451c98050f3d4fead3f9a37bbf6b2bc1a78
NaiduBhavana <123732813+NaiduBhavana@users.noreply.github.com>
2024-08-11 09:55:19 +0530

    Add files via upload

---
+++ S06L7Default_Constructor/Main.java
@@ -0,0 +1,12 @@
+package org.studyeasy.com;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Smartphone phone = new Smartphone();
+		System.out.println(phone.getbrand());
+		
+	}
+
+}
---
+++ S06L7Default_Constructor/Smartphone.java
@@ -0,0 +1,12 @@
+package org.studyeasy.com;
+
+public class Smartphone 
+{
+	private String brand = "Apple";
+	public String getbrand()
+	{
+		return brand;
+	}
+}
+//Inbetween default constructor and parameterized constructor priority is more for 
+//parameterized constructor.
