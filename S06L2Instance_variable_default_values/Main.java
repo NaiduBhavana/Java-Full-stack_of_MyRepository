bf7a7b1c715f9d6968b0158ee422fe237aa9996c
NaiduBhavana <123732813+NaiduBhavana@users.noreply.github.com>
2024-08-11 09:08:51 +0530

    Add files via upload

---
+++ S06L2Instance_variable_default_values/Main.java
@@ -0,0 +1,19 @@
+package org.studyeasy;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Car c = new Car();
+		System.out.println(c.doors);
+		System.out.println(c.engine_name);
+		System.out.println(c.state);
+//		String f;
+//		System.out.println(f);
+
+	}
+
+}
+//Here instance variables (i.e.,car class variables)are having default values.Those are
+//0 for int,null for string,false for boolean
