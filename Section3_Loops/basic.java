c8e18946d51d014f9206742d0f223ad5c532841a
NaiduBhavana <123732813+NaiduBhavana@users.noreply.github.com>
2024-08-11 10:18:26 +0530

    Add files via upload

---
+++ Section3_Loops/basic.java
@@ -0,0 +1,16 @@
+package Section3_Loops;
+import java.io.*;
+import java.util.*;
+public class basic 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3\n4\n5\n6");
+		//Here we can use \n in one single printf statement
+
+	}
+
+}
---
+++ Section3_Loops/infinite_loop.java
@@ -0,0 +1,16 @@
+package Section3_Loops;
+import java.io.*;
+import java.util.*;
+public class infinite_loop 
+{
+
+	public static void main(String[] args) 
+	{
+		for(;;)
+		{
+			System.out.println("......");
+		}
+		//Here this loop runs infinite times as we haven't mentioned any condition.
+	}
+
+}
